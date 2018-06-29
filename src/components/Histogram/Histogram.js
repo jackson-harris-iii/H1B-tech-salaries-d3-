562be356fb7f3900aa1c5192bafa49ebf09f311f
Jackson Harris III <Apollo@Apollo.localdomain>
2018-06-28 19:28:43 -0700

    render left side axis

--- src/components/Histogram/Histogram.js
+++ src/components/Histogram/Histogram.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 import HistogramBar from './HistogramBar'
+import Axis from './Axis'
 
 class Histogram extends Component {
     
@@ -62,7 +63,12 @@ class Histogram extends Component {
                 <g className="bars"> 
                     {bars.map(this.makeBar.bind(this))}
                 </g> 
-            </g>
+                
+                <Axis x={this.props.axisMargin-3}
+                    y={0}
+                    data={bars}
+                    scale={this.yScale} />
+            </g>        
         );
     
     }
