44d0993c6f4a778e630848fc3668cb4337fef7fe
Jackson Harris III <Apollo@Apollo.localdomain>
2018-06-28 19:34:11 -0700

    init Title componenet

---
+++ src/components/Meta/Title.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { scaleLinear } from 'd3-scale';
+import { mean as d3mean, extent as d3extent } from 'd3-array';
+
+import USStatesMap from './USStatesMap';
+
+class Title extends Component {
+    
+    get yearsFragment() {
+
+    }
+
+    get USstateFragment() {
+
+    }
+
+    get jobTitleFragment() {
+
+    }
+
+    get format() {
+
+    }
+
+    render() {
+
+    }
+}    
+
+export default Title
